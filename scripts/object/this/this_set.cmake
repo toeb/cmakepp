864bedb9166d75219468e9e5c10e4b760579e307
Tobias Becker <beckert@sim.informatik.tu-darmstadt.de>
2014-02-25 02:08:17 +0100

    removed some debug messages for performance reasons

--- scripts/object/this/this_set.cmake
+++ scripts/object/this/this_set.cmake
@@ -2,7 +2,7 @@
 macro(this_set member_name)
 	this_check()
 	obj_set(${this} ${member_name} "${ARGN}")
-	debug_message("setting ${member_name} to ${ARGN}")
+	#debug_message("setting ${member_name} to ${ARGN}")
 	# todo.  only one of the two should be used....... this causes problems depending on which in which context this is called
 	set(${member_name} "${ARGN}" PARENT_SCOPE)
 	set(${member_name} "${ARGN}")
--- scripts/stack/peek_front.cmake
+++ scripts/stack/peek_front.cmake
@@ -2,5 +2,5 @@
 macro(peek_front var result)
 	get_property(lst GLOBAL PROPERTY ${var})
 	list(GET lst 0 ${result})
-	debug_message("peeking frons ${result}")
+	#debug_message("peeking frons ${result}")
 endmacro()
