1f70f108af54f3680da248d12847d65c9d10e48f
Tobias Becker <becker.tobi@gmail.com>
2014-03-02 22:53:48 +0100

    added global var accessors, added map_equal and map_issubsetof added list_isinorder

--- tests/map_equal.cmake
+++ tests/map_equal.cmake
@@ -10,12 +10,12 @@ function(test)
 	map_append(${uut} k3 "asd asda asd")
 	
 	map_create(uut2)
+	map_append(${uut2} k2 c)
+	map_append(${uut2} k2 c)
 	map_append(${uut2} k1 a)
 	map_append(${uut2} k1 b)
 	map_append(${uut2} k1 c)
 	
-	map_append(${uut2} k2 c)
-	map_append(${uut2} k2 c)
 
 	map_append(${uut2} k3 "asd asda asd")
 	
