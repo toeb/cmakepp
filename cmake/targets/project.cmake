1f2392a0d5cf9058fc439caf441ae43fc72743d4
Tobias Becker <beckert@sim.informatik.tu-darmstadt.de>
2014-05-13 04:25:18 +0200

    ...

--- cmake/targets/project.cmake
+++ cmake/targets/project.cmake
@@ -3,4 +3,8 @@
 macro(project name)
   _project(${name} ${ARGN})
   message(DEBUG LEVEL 4 "registering project '${name}'")
+  map_new()
+  ans(cmake_current_project)
+  map_set(${cmake_current_project} name "${name}")
+  map_set(${cmake_current_project} directory "${CMAKE_CURRENT_LIST_DIR}")
 endmacro()
