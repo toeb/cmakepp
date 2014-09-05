466926e42e6a3d00c37b445eff5c61e421276492
Tobias Becker <becker.tobi@gmail.com>
2014-09-05 16:30:29 +0200

    fixed string_substring

--- cmake/string/string_substring.cmake
+++ cmake/string/string_substring.cmake
@@ -1,15 +1,13 @@
 # wraps the substring command
 # optional parameter end 
 function(string_substring str start)
-  set(end ${ARGN})
-  if(NOT end)
-    set(end -1)
+  set(len ${ARGN})
+  if(NOT len)
+    set(len -1)
   endif() 
   string_normalize_index("${str}" "${start}")
   ans(start)
-  string_normalize_index("${str}" "${end}")
-  ans(end)
 
-  string(SUBSTRING "${str}" "${start}" "${end}" res)
+  string(SUBSTRING "${str}" "${start}" "${len}" res)
   return_ref(res)
 endfunction()
