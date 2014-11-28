98ebc5ef3ad01f459bbd586b2da5250a08c8b81e
Tobias Becker <becker.tobi@gmail.com>
2014-11-29 00:57:33 +0100

    millis updated command to work under unix/gcc

--- cmake/timer/timer_start.cmake
+++ cmake/timer/timer_start.cmake
@@ -18,12 +18,21 @@ function(timer_start id)
   ## (the time it takes from timer_start to timer_elapsed to run)
   ## this prejudice value is then subtracted everytime elapse is run
   ## thus minimizing the error
-  timer_start(initial)  
-  timer_elapsed(initial)
 
-  ans(prejudice)
+  #foreach(i RANGE 0 3)
+    timer_start(initial)  
+    timer_elapsed(initial)
+    ans(prejudice)
+
+    map_tryget(__timers __prejudice)
+    ans(pre)
+    math(EXPR prejudice "${prejudice} + ${pre}")
+    map_set_hidden(__timers __prejudice ${prejudice})
+  #endforeach()
+
+
   timer_delete(initial)
-  map_set_hidden(__prejudice ${prejudice})
+
 
   return_reset()
   timer_start("${id}")
--- tests/compile_command_test.cmake
+++ tests/compile_command_test.cmake
@@ -2,17 +2,17 @@ function(test)
 
 
 
-    timer_start(mytimer)
-    timer_start(mysecondtimer)
-    timer_elapsed(mysecondtimer)
-    ans(res)
+  timer_start(mytimer)
+  timer_start(mysecondtimer)
+  timer_elapsed(mysecondtimer)
+  ans(res)
 
-    message("${res} ms")
-
-
+  message("${res} ms")
 
 
-    timers_print_all()
+
+
+  timers_print_all()
 
 
 endfunction()
