0dbbdbeb8088f0900bdebbd06c9facddce8f2dd8
Tobias Becjer <becker.tobi@gmail.com>
2014-12-19 04:45:49 +0100

    fixed get_function_string to also work with lambdas

--- tests/list/list_count_test.cmake
+++ tests/list/list_count_test.cmake
@@ -1,18 +1,5 @@
 function(test)
 
-## counts all element for which the element hold 
-function(list_count lst predicate)
-  function_import("${predicate}" as predicate_function REDEFINE)
-  set(ct 0)
-  foreach(item ${${lst}})
-    predicate_function("${item}")
-    ans(match)
-    if(match)
-      math(EXPR ct "${ct} + 1") 
-    endif()
-  endforeach()
-  return("${ct}")
-endfunction()
 
 
   index_range(0 10)
