56e74decfd249926b5fe7d28c11d3cb15d6f2b07
Tobias Becjer <becker.tobi@gmail.com>
2015-02-03 16:24:16 +0100

    updated after_sucess script

--- build/after_success.cmake
+++ build/after_success.cmake
@@ -1,8 +1,9 @@
 # compiles cmake to the specified target directory
 # call this script with cmake -P compile.cmake path/to/target/dir
-include("${CMAKE_CURRENT_LIST_DIR}/../cmakpp.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/../cmakepp.cmake")
 
 
-compile_oocmake("${CMAKE_CURRENT_LIST_DIR}" "Findcmakepp.cmake")
+compile_oocmake("${CMAKE_CURRENT_SOURCE_DIR}" "${CMAKE_CURRENT_SOURCE_DIR}/release/FindCMakepp.cmake")
+
 
 
