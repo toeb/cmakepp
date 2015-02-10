b8e8dfa7bb74134cb5058866451076086d70f572
Tobias Becjer <becker.tobi@gmail.com>
2015-02-10 21:11:08 +0100

    fixed 0 char warning in cmake 3.1

--- cmake/string/cmake_string_unescape.cmake
+++ cmake/string/cmake_string_unescape.cmake
@@ -11,7 +11,7 @@
     string(REPLACE "\\;" ";"  str "${str}")
     string(REPLACE "\\n" "\n" str "${str}")
     string(REPLACE "\\r" "\r" str "${str}")
-    string(REPLACE "\\0" "\0" str "${str}")
+    string(REPLACE "\\0" "" str "${str}") ## not supported  in cmake strings
     string(REPLACE "\\ " " " str "${str}")
     return_ref(str)
   endfunction()
