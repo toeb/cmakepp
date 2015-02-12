bda32eeb67499fd74b03554137b66ebe6000f144
Tobias Becjer <becker.tobi@gmail.com>
2015-02-12 15:58:05 +0100

    added a hack so that inclusion order does nto matter

--- cmake/task/task_enqueue.cmake
+++ cmake/task/task_enqueue.cmake
@@ -1,5 +1,13 @@
 ## adds a callable as a task which is to be invoked later
 function(task_enqueue callable)  
+  if(NOT COMMAND return_ref OR NOT COMMAND function_import OR NOT COMMAND ans OR NOT COMMAND map_new OR NOT COMMAND map_set OR NOT COMMAND map_append)
+    ## hack. sometimes task_enqueue is called before function_import
+    ## semicolon encode before string_semicolon_encode exists
+    string(ASCII  31 us)
+    string(REPLACE ";" "${us}" callable "${callable}")
+    set_property(GLOBAL APPEND PROPERTY __initial_invoke_later_list "${callable}") 
+    return()    
+  endif()
   function_import("${callable}")
   ans(callback)
   map_new()
