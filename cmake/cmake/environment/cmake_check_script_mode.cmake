aa1194fb963269e9ded562fac3d3f86da4b9511a
Tobias Becker <becker.tobi@gmail.com>
2017-06-14 00:35:46 +0200

    added empty element respecting list pad and list pad set

--- cmake/cmake/environment/cmake_check_script_mode.cmake
+++ cmake/cmake/environment/cmake_check_script_mode.cmake
@@ -9,13 +9,3 @@ function(cmake_check_script_mode)
   return()
 endfunction()
 
-
-## causes an error if cmake is not in script mode
-function(cmake_check_config_mode)
-  cmake_is_script_mode()
-  ans(isScriptMode)
-  if(isScriptMode)
-    message(FATAL_ERROR "cmake needs to be in config mode! - aborting execution")
-  endif()
-  return()
-endfunction()
